0e26b1f8704ab45e4902ea2e8e58f2bb228d212f
Chase Crowder <gcrowder93@gmail.com>
2021-12-14 17:29:55 -0800

    deleted everything because all of a sudden none of it makes sense

--- src/App.js
+++ src/App.js
@@ -1,75 +1,14 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
-
-import CharactersList from './views/Characters/CharactersList';
-import FilmsList from './views/Films/FilmsList';
-import BooksList from './views/Books/BooksList';
-import { fetchBooks } from './services/books';
-import { fetchCharacters } from './services/characters';
+import CharactersList from './components/CharactersList';
+import FilmsList from './components/FilmsList';
+import BooksList from './components/BooksList';
 
 function App() {
   const [films, setFilms] = useState([]);
   const [characters, setCharacters] = useState([]);
   const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    getFilms();
-    getCharacters();
-    getBooks();
-  }, []);
-
-  const getBooks = async () => {
-    const resp = await fetch(`&{process.env.REACT_APP_SUPABASE_URL}/rest/v1/books`, {
-      headers: {
-        apikey: process.env.REACT_APP_SUPABASE_KEY,
-        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-      },
-    });
-    const data = await resp.json();
-    const bookData = data.map((item) => [item.title]);
-    setBooks(bookData);
-    return [BooksList];
-  };
-  // console.log(BooksList);
-
-  const getFilms = async () => {
-    const resp = await fetch(`&{process.env.REACT_APP_SUPABASE_URL}/rest/v1/films`, {
-      headers: {
-        apikey: process.env.REACT_APP_SUPABASE_KEY,
-        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-      },
-    });
-
-    const data = await resp.json();
-    const filmData = data.map((item) => [
-      item.title,
-      item.title.toLowerCase().replace(/ /g, '-'),
-      item.box_office_total,
-      item.academy_award_nominations,
-    ]);
-    setFilms(filmData);
-    return [FilmsList];
-  };
-
-  const getCharacters = async () => {
-    const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/characters`, {
-      headers: {
-        apikey: process.env.REACT_APP_SUPABASE_KEY,
-        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-      },
-    });
-    const data = await resp.json();
-    const characterData = data.map((item) => ({
-      name: item.name,
-      birth: item.birth,
-      death: item.death,
-      dates: item.birth === item.death ? 'Unknown' : `${item.birth} - ${item.death}`,
-    }));
-
-    setCharacters(characterData);
-    return [CharactersList];
-  };
 
   return (
     <div className="App">
@@ -102,3 +41,62 @@ function App() {
 }
 
 export default App;
+
+// useEffect(() => {
+//   getFilms();
+//   getCharacters();
+//   getBooks();
+//   fetchFilms();
+// }, []);
+
+// const getBooks = async () => {
+//   const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/books`, {
+//     headers: {
+//       apikey: process.env.REACT_APP_SUPABASE_KEY,
+//       Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
+//     },
+//   });
+//   const data = await resp.json();
+//   const bookData = data.map((item) => [item.title]);
+//   setBooks(bookData);
+//   return [BooksList];
+// };
+// console.log(books);
+
+// const getFilms = async () => {
+//   const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/films`, {
+//     headers: {
+//       apikey: process.env.REACT_APP_SUPABASE_KEY,
+//       Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
+//     },
+//   });
+
+//   const data = resp.json();
+//   const filmData = data.map((item) => [
+//     item.title,
+//     item.title.toLowerCase().replace(/ /g, '-'),
+//     item.box_office_total,
+//     item.academy_award_nominations,
+//   ]);
+//   setFilms(filmData);
+//   return [FilmsList];
+// };
+
+// const getCharacters = async () => {
+//   const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/characters`, {
+//     headers: {
+//       apikey: process.env.REACT_APP_SUPABASE_KEY,
+//       Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
+//     },
+//   });
+//   const data = await resp.json();
+//   const characterData = data.map((item) => ({
+//     name: item.name,
+//     birth: item.birth,
+//     death: item.death,
+//     dates: item.birth === item.death ? 'Unknown' : `${item.birth} - ${item.death}`,
+//   }));
+
+//   setCharacters(characterData);
+//   return [CharactersList];
+// };
